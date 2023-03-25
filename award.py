b8331bfb2d40e70978612d2fa67689aaedf56d20
Ange Michael <67484535+michaelatsey@users.noreply.github.com>
2023-03-25 18:42:53 +0100

    update

--- award.py
+++ award.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
