8667014691f6241c32b88b8db7397cd7381e2a3e
Ange Michael <67484535+michaelatsey@users.noreply.github.com>
2023-03-25 18:52:49 +0100

    dzd

--- award.py
+++ award.py
@@ -23,6 +23,10 @@ def vote():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--no-zygote")
+    options.add_argument("--single-process")
 
     # Configuration de Selenium et ouverture du navigateur
     #driver = webdriver.Chrome(options=options)
